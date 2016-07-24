6137d976595b35c45a52f72c628e862602edb7d8
YoshiRulz <yoshiisawsm@gmail.com>
2016-07-24 11:48:30 +1000

    Added body content

---
+++ about/body.php
@@ -0,0 +1,5 @@
+<p>
+Welcome to the future &mdash; the future where society is built upon science, data and currency are interchangable, and robots have enslaved mankind. Maybe not that last one.<br>
+As a self-managed team, we engage our minds with other enlightened youth in various ways, including competing in the <abbr title="For Inspiration and Recognition of Science and Technology">FIRST</abbr><sup>&reg;</sup> Robotics Challenge and FIRST<sup>&reg;</sup> Tech Challenge, to advance the creativity and outreach of technology, and also to encourage the next generation to transcend the boundaries of today.<br>
+Want to learn more about FIRST&reg;? You can read about their <a href="http://www.firstinspires.org/about/vision-and-mission">vision and mission</a> on their website, <a href="firstinspires.org">firstinspires.org</a>.
+</p>
