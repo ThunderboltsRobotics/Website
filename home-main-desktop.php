9296027ebc161a23fe4c3e8f17ff36e9de8bd803
YoshiRulz <yoshiisawsm@gmail.com>
2016-07-29 18:12:49 +1000

    Reordered links, changed link target attr

--- home-main-desktop.php
+++ home-main-desktop.php
@@ -1,15 +1,20 @@
 <main>
 	<aside class="nav-col" style="align-items: flex-end; flex: 1.62;">
-		<nav><a href="about">About</a></nav>
-		<nav><a href="the-team">The Team</a></nav>
-		<nav><a href="software">Software</a></nav>
+		<nav><a href="about" target="_top">About</a></nav>
+		<nav><a href="the-team" target="_top">The Team</a></nav>
+		<nav><a href="the-robot" target="_top">The Robot</a></nav>
+		<nav><a href="software" target="_top">Software</a></nav>
 	</aside>
 	<section class="nav-col" style="align-items: center; flex: 1;">
 		<?php require_once "favicon/original.svg"; ?>
 	</section>
 	<aside class="nav-col" style="align-items: flex-start; flex: 1.62;">
-		<nav><a href="sponsors">Sponsors</a></nav>
-		<nav><a href="history">History</a></nav>
-		<nav><a href="the-robot">The Robot</a></nav>
+		<nav><a href="sponsors" target="_top">Sponsors</a></nav>
+		<nav><a href="history" target="_top">History</a></nav>
+		<!-- <nav>
+			<a href="https://www.youtube.com/channel/UCNCmshpL19mBLa_LTZ8EzaA"><img height="16" src="/static/images/yt-icon.png" width="16"></a>
+			<a href=""><img height="16" src="/static/images/fb-icon.png" width="16"></a>
+			<a href=""><img height="16" src="/static/images/gplus-icon.png" width="16"></a>
+		</nav> -->
 	</aside>
 </main>
