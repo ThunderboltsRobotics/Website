5260e46c2047afc1f2da3f1d9a0a446a18f44971
YoshiRulz <yoshiisawsm@gmail.com>
2016-07-29 18:11:55 +1000

    Fixed mobile and desktop being switched

--- body.php
+++ body.php
@@ -1,7 +1,7 @@
 <?php
 if (stripos($_SERVER["HTTP_USER_AGENT"], "mobile")) {
-	require_once "home-main-desktop.php";
-} else {
 	require_once "home-main-mobile.php";
+} else {
+	require_once "home-main-desktop.php";
 }
 ?>
