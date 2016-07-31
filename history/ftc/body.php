60dec5c9b0248f877d26330e74fb3ecb20634c39
YoshiRulz <yoshiisawsm@gmail.com>
2016-07-31 13:20:33 +1000

    FTC history

---
+++ history/ftc/body.php
@@ -0,0 +1,18 @@
+<section>
+	<table border="1" style="margin: auto;">
+		<tr>
+			<th>Season</th>
+			<td></td>
+		</tr>
+		<?php
+		$data = json_decode(file_get_contents("../history.json"), true)["FTC"];
+		for ($y = date("Y"); $y >= $data["rookie-year"]; $y--) {
+			$f = "$y/" . (substr($y, -2) + 1);
+			echo "<tr>\n";
+			echo "<td>$f</td>\n";
+			echo "<td>" . (isset($data[$f]) ? $data[$f] : "N/A") . "</td>\n";
+			echo "</tr>\n";
+		}
+		?>
+	</table>
+</section>
