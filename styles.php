e33c4575362db3ec147b2ffcbf1d65fa6f85ee5e
YoshiRulz <yoshiisawsm@gmail.com>
2016-07-31 13:35:04 +1000

    Split homepage styles to mobile and desktop files

--- styles.php
+++ styles.php
@@ -10,27 +10,10 @@ main {
 a {
 	text-decoration: inherit;
 }
-<?php if (stripos($_SERVER["HTTP_USER_AGENT"], "mobile")) { ?>
-/* TODO */
-<?php } else { ?>
-main, #social-icons {
-	display: flex;
-	flex-flow: row nowrap;
+<?php
+if (stripos($_SERVER["HTTP_USER_AGENT"], "mobile")) {
+	require_once "home-styles-mobile.css";
+} else {
+	require_once "home-styles-desktop.css";
 }
-.nav-col, .nav-col > nav {
-	display: flex;
-	flex-flow: column nowrap;
-	justify-content: center;
-}
-.nav-col > nav {
-	margin: 1em;
-}
-.nav-col > nav > a {
-	font-family: Audiowide;
-	font-size: 3em;
-	font-style: italic;
-}
-#social-icons > a {
-	margin: 0.25em;
-}
-<?php } ?>
+?>
