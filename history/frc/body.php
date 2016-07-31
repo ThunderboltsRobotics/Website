da4f173639f7f1996a5076d4b2367e5bebcd4953
YoshiRulz <yoshiisawsm@gmail.com>
2016-07-31 13:20:40 +1000

    FRC history

---
+++ history/frc/body.php
@@ -0,0 +1,17 @@
+<section>
+	<table border="1" style="margin: auto;">
+		<tr>
+			<th>Season</th>
+			<td></td>
+		</tr>
+		<?php
+		$data = json_decode(file_get_contents("../history.json"), true)["FRC"];
+		for ($y = date("Y"); $y >= $data["rookie-year"]; $y--) {
+			echo "<tr>\n";
+			echo "<td>$y</td>\n";
+			echo "<td>" . (isset($data["$y"]) ? $data["$y"] : "N/A") . "</td>\n";
+			echo "</tr>\n";
+		}
+		?>
+	</table>
+</section>
