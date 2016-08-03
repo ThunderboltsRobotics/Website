74a5b4df13fdf92fa6fe05fe88d4c827a778c84a
YoshiRulz <yoshiisawsm@gmail.com>
2016-08-03 19:18:33 +1000

    Added more to the about page

--- about/body.php
+++ about/body.php
@@ -1,5 +1,23 @@
-<p>
-Welcome to the future &mdash; the future where society is built upon science, data and currency are interchangable, and robots have enslaved mankind. Maybe not that last one.<br>
-As a self-managed team, we engage our minds with other enlightened youth in various ways, including competing in the <abbr title="For Inspiration and Recognition of Science and Technology">FIRST</abbr><sup>&reg;</sup> Robotics Challenge and FIRST<sup>&reg;</sup> Tech Challenge, to advance the creativity and outreach of technology, and also to encourage the next generation to transcend the boundaries of today.<br>
-Want to learn more about FIRST&reg;? You can read about their <a href="http://www.firstinspires.org/about/vision-and-mission">vision and mission</a> on their website, <a href="firstinspires.org">firstinspires.org</a>.
-</p>
+<section>
+	<p>
+	Welcome to the future &mdash; the future where society is built upon science, data and currency are interchangable, and robots have enslaved mankind. Maybe not that last one.<br>
+	As a self-managed team, we engage our minds with other enlightened youth in various ways, including competing in the <abbr title="For Inspiration and Recognition of Science and Technology">FIRST</abbr><sup>&reg;</sup> Robotics Challenge and FIRST<sup>&reg;</sup> Tech Challenge, to advance the creativity and outreach of technology, and also to encourage the next generation to transcend the boundaries of today.
+	</p>
+<section>
+<table style="margin: auto;">
+	<tr>
+		<th>About the team</th>
+		<th>About FIRST&reg;</th>
+	</tr>
+	<tr>
+		<td>
+			<p>
+			</p>
+		</td>
+		<td>
+			<p>
+			Want to learn more about FIRST&reg;? You can read about their <a href="http://www.firstinspires.org/about/vision-and-mission">vision and mission</a> on their website, <a href="firstinspires.org">firstinspires.org</a>.
+			</p>
+		</td>
+	</tr>
+</table>
