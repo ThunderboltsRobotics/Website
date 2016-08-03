399efef900188dd7468e68d91c10c796a08e88f6
YoshiRulz <yoshiisawsm@gmail.com>
2016-08-03 19:15:24 +1000

    The images on the history page are now more obviously links

--- history/body.php
+++ history/body.php
@@ -1,11 +1,10 @@
-<section>
-	<table style="margin: auto;">
-		<tr>
-			<th colspan="2">FIRST&reg; Competition History</th>
-		</tr>
-		<tr>
-			<td><a href="frc" target="_top"><img alt="FIRST Robotics Competition" src="/static/images/FRC-banner.png" width="201"></a></td>
-			<td><a href="ftc" target="_top"><img alt="FIRST Tech Challenge" src="/static/images/FTC-banner.png" width="186"></a></td>
-		</tr>
-	</table>
-</section>
+<main>
+	<a href="frc" target="_top">
+		<img alt="FIRST Robotics Competition" src="/static/images/FRC-banner.png" width="201">
+		<p>FRC Competition History</p>
+	</a>
+	<a href="ftc" target="_top">
+		<img alt="FIRST Tech Challenge" src="/static/images/FTC-banner.png" width="186">
+		<p>FTC Competition History</p>
+	</a>
+</main>
--- history/styles.php
+++ history/styles.php
@@ -1,3 +1,13 @@
-td, th, tr {
+main {
+	display: flex;
+	justify-content: center;
+	margin: auto;
+}
+main > * {
+	border: 0.125em solid black;
+	margin: 1em;
+	padding: 0.5em;
+}
+td, th {
 	padding: 0.5em;
 }
